3bb2087823e26b9cdd6d16a47bbb171790706d18
Manuel Velez <2113020404@untels.edu.pe>
2024-07-01 17:13:24 -0500

    Parte 2 - Tabla Estudiante - OK

--- VVR_UC04/miapp/models.py
+++ VVR_UC04/miapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class estudiante(models.Model):
+    codigo = models.CharField(max_length=100)
+    dni = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    apepat = models.CharField(max_length=100)
+    apemat = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    estado = models.BooleanField()
